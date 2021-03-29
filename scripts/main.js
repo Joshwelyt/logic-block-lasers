e50e16ec4064baf81336b0170a83027347d51323
NiChrosia <nichrosian@gmail.com>
2021-03-29 13:44:36 -0500

    Optimisations to the code, now iterates over blocks in view, rather than everything.

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,11 @@ const color = Color.valueOf("a794e3")
 // Logic Block Laser effect
 
 function logicLasers() {
-	Groups.build.each(b => {
+	let width = Core.graphics.getWidth() / 7.5;
+	let height = Core.graphics.getHeight() / 7.4;
+	let screenSize = 1 / Vars.renderer.getDisplayScale();
+	
+	Vars.indexer.eachBlock(Vars.player.team(), Vars.player.x, Vars.player.y, ((screenSize * width + screenSize * height) / 2) * Vars.tilesize, b => b.block.class.getSuperclass() == LogicBlock, b => {
 		if (b.block.class.getSuperclass() == LogicBlock) {
 			b.links.each(l => {
 				let linkBuild = Vars.world.tile(l.x, l.y).build;
